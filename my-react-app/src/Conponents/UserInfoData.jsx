bb080f6f6b678051ccde7f82c46d3e915fca4dee
Yoni Klein <yonatanklein058@gmail.com>
2024-09-10 22:56:37 +0300

    fix

--- my-react-app/src/Conponents/UserInfoData.jsx
+++ my-react-app/src/Conponents/UserInfoData.jsx
@@ -4,15 +4,15 @@ function UserInfoData({user}) {
   return (
     <div className="user-info-container">      
     <div >
-      <label>First Name:</label>
+      <label className="label-user">First Name:</label>
       <p>{user.first_name}</p>
     </div>
     <div >
-      <label>Last Name:</label>
+      <label className="label-user">Last Name:</label>
       <p>{user.last_name}</p>
     </div>
     <div >
-      <label>Email:</label>
+      <label className="label-user">Email:</label>
       <p>{user.email}</p>
     </div>
     </div>
